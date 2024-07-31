6153202c6475f161d2987fe8ca2d45ea53268770
WZJun <128673164+OSshiyanGuoGuoGuo@users.noreply.github.com>
2024-07-31 17:23:31 +0800

    Add files via upload

---
+++ L2.java
@@ -0,0 +1,27 @@
+public class ReptileAndFish {
+
+    public static class Reptile {
+        private String name;
+
+        public Reptile(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+    }
+
+    public static class Fish {
+        private String name;
+
+        public Fish(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+    }
+
+}
