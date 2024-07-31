8cf4cbc9fd40cad452482e6a899a322a142ed436
WZJun <128673164+OSshiyanGuoGuoGuo@users.noreply.github.com>
2024-07-31 16:57:44 +0800

    Add files via upload

---
+++ L1.java
@@ -0,0 +1,19 @@
+public class L1 {
+    public String location;
+    public String address;
+
+    public foo(){
+        System.out.println(address);
+    }
+    public bar(){
+        System.out.println(address);
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+}
